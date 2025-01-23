23c7bf4c53d499a472f33aa8b41ac7de5f72461c
manish132 <125593236+Manish13122@users.noreply.github.com>
2025-01-23 15:42:02 +0530

    updated links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -7,33 +8,33 @@ const Footer = () => {
         <div className="container mx-auto px-4">
           {/* Footer Top Section: Navigation Links */}
           <div className="flex justify-center space-x-8 mb-4">
-            <a href="/" className="hover:text-blue-400 transition-colors">Home</a>
-            <a href="/about" className="hover:text-blue-400 transition-colors">About</a>
-            <a href="/profile" className="hover:text-blue-400 transition-colors">Profiles</a>
-            <a href="/contact" className="hover:text-blue-400 transition-colors">Contact Us</a>
+            <NavLink to="/" className="hover:text-blue-400 transition-colors">Home</NavLink>
+            <NavLink to="/about" className="hover:text-blue-400 transition-colors">About</NavLink>
+            <NavLink to="/profile" className="hover:text-blue-400 transition-colors">Profiles</NavLink>
+            <NavLink to="/contact" className="hover:text-blue-400 transition-colors">Contact Us</NavLink>
           </div>
 
-          {/* Footer Middle Section: Social Media Links */}
+          {/* Footer Middle Section: Social MediNavLink Links */}
           <div className="flex justify-center space-x-6 mb-4">
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
+            <NavLink to="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
               <i className="fab fa-linkedin fa-lg"></i>
-            </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
+            </NavLink>
+            <NavLink to="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
               <i className="fab fa-twitter fa-lg"></i>
-            </a>
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
+            </NavLink>
+            <NavLink to="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800">
               <i className="fab fa-facebook fa-lg"></i>
-            </a>
-            <a href="mailto:info@eliteca.com" className="text-blue-600 hover:text-blue-800">
+            </NavLink>
+            <NavLink to="mailto:info@eliteca.com" className="text-blue-600 hover:text-blue-800">
               <i className="fas fa-envelope fa-lg"></i>
-            </a>
+            </NavLink>
           </div>
 
           {/* Footer Bottom Section: Copyright and Additional Info */}
           <div className="text-center mt-4">
             <p>&copy; 2025 EliteCA. All rights reserved.</p>
             <p className="text-sm mt-2">
-              Designed with <span className="text-red-500">&hearts;</span> by EliteCA Team
+              Designed with <span className="text-red-500">&hearts;</span> by EliteCNavLink Team
             </p>
           </div>
         </div>
